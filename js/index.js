cff93a67aaabbc68722f0d0339f717fdef25c91c
e-nk <enocklagat747@gmail.com>
2022-12-15 10:29:35 +0300

    Added html elements on DOM

--- js/index.js
+++ js/index.js
@@ -16,8 +16,45 @@ const updatePlaylist=()=>{
             //console.log(data.results);
             const artists = data.results;
             return artists.map(result =>{
-                
+
+                //Create HTML Elements
+                const article = document.createElement('article'),
+                        artists = document.createElement('p'),
+                        song = document.createElement('h4'),
+                        img = document.createElement('img'),
+                        audio = document.createElement('audio'),
+                        audioSource = document.createElement('source')
+                        
+                //Adding content to the elements
+                artists.innerHTML = result.artistName;
+                    song.innerHTML = result.trackName;
+                    img.src = result.artworkUrl100;
+                    audioSource.src = result.previewUrl;
+                    audio.controls = true;
+
+                    article.appendChild(img);
+                    article.appendChild(artists);
+                    article.appendChild(song);
+                    article.appendChild(audio);
+                    audio.appendChild(audioSource);
+
+                    songCards.appendChild(article);                
+
             })
         })
+        .catch((error) =>console.log('Failed to request resource: ' + error))
     }
+
 }
+
+const searchButton = document.getElementById('searchBtn');
+searchButton.addEventListener('click', updatePlaylist)
+
+document.addEventListener('play', event => {
+    const audio = document.getElementsByTagName('audio');
+    for (let i = 0; i < audio.length; i++) {
+        if (audio[i] != event.target){
+            audio.pause();
+        }
+    }
+})
