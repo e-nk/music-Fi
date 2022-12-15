0c02331e00688bbca0ae82a76afe610dc6363c30
e-nk <enocklagat747@gmail.com>
2022-12-15 12:31:42 +0300

    Functionalities added

--- js/index.js
+++ js/index.js
@@ -1,32 +1,33 @@
-let playlist= ''
-const updatePlaylist=()=>{
-    playlist = document.getElementById('search').value;
-    //now we check if the song or podcast exists
-    if(!playlist || playlist === ''){
-        alert('Search box cannot be empty!');
-    } else{
-        const url =`https://itunes.apple.com/search?term=${playlist}`;
-        const songCards = document.getElementById('songs');
-        while(songCards.firstChild){
-            songCards.removeChild(songCards.firstChild);
+let term = '';
+const updateTerm = () => {
+    term = document.getElementById('searchTerm').value;
+    // check term exist
+    if (!term || term === '') {
+        alert('Please enter a seach term');
+    } else {
+        const url = `https://itunes.apple.com/search?term=${term}`;
+        const songContainer = document.getElementById('songs');
+        while (songContainer.firstChild) {
+            songContainer.removeChild(songContainer.firstChild);
         }
         fetch(url)
-        .then((Response) => Response.json())
-        .then((data) => {
-            //console.log(data.results);
-            const artists = data.results;
-            return artists.map(result =>{
+            .then((Response) => Response.json())
+            .then((data) => {
+                // console.log(data.results);
+                const artists = data.results;
+                return artists.map(result => {
+                    // Now create Html Element 
 
-                //Create HTML Elements
-                const article = document.createElement('article'),
+                    const article = document.createElement('article'),
                         artists = document.createElement('p'),
                         song = document.createElement('h4'),
                         img = document.createElement('img'),
                         audio = document.createElement('audio'),
                         audioSource = document.createElement('source')
-                        
-                //Adding content to the elements
-                artists.innerHTML = result.artistName;
+
+                    // Now put content 
+
+                    artists.innerHTML = result.artistName;
                     song.innerHTML = result.trackName;
                     img.src = result.artworkUrl100;
                     audioSource.src = result.previewUrl;
@@ -38,23 +39,21 @@ const updatePlaylist=()=>{
                     article.appendChild(audio);
                     audio.appendChild(audioSource);
 
-                    songCards.appendChild(article);                
-
+                    songContainer.appendChild(article);
+                })
             })
-        })
-        .catch((error) =>console.log('Failed to request resource: ' + error))
+            .catch(error => console.log('Request failed:', error))
     }
-
 }
 
-const searchButton = document.getElementById('searchBtn');
-searchButton.addEventListener('click', updatePlaylist)
+const searchBtn = document.getElementById('searchTermBtn');
+searchBtn.addEventListener('click', updateTerm)
 
 document.addEventListener('play', event => {
     const audio = document.getElementsByTagName('audio');
     for (let i = 0; i < audio.length; i++) {
-        if (audio[i] != event.target){
-            audio.pause();
+        if (audio[i] != event.target) {
+            audio[i].pause();
         }
     }
-})
+}, true)
